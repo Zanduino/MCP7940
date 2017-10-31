d291a61d064167ac3bb88896e89630ef5fd7c58f
Arnd <SV-Zanshin@users.noreply.github.com>
2017-10-31 16:32:46 +0100

    Bug #6 fix

--- MCP7940.h
+++ MCP7940.h
@@ -21,7 +21,7 @@
 **                                                                                                                **
 ** Vers.  Date       Developer                     Comments                                                       **
 ** ====== ========== ============================= ============================================================== **
-** 1.0.5  2017-10-31 https://github.com/SV-Zanshin Bug #6 to remove classification on 2 template functions        **
+** 1.0.5a 2017-10-31 https://github.com/SV-Zanshin Bug #6 to remove classification on 2 template functions        **
 ** 1.0.4c 2017-08-13 https://github.com/SV-Zanshin Enhancement #5 to remove checks after Wire.requestFrom()       **
 ** 1.0.4b 2017-08-08 https://github.com/SV-Zanshin Replaced readRAM and writeRAM with template functions          **
 ** 1.0.4a 2017-08-06 https://github.com/SV-Zanshin Removed unnecessary MCP7940_I2C_Delay and all references       **
