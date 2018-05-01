0380b5bb7184c50d23838d1467263fd08b3994ec
Arnd <SV-Zanshin@users.noreply.github.com>
2018-05-01 18:56:23 +0200

    Corrected spelling

--- Examples/SetAlarms/SetAlarms.ino
+++ Examples/SetAlarms/SetAlarms.ino
@@ -63,7 +63,7 @@ void setup() {                                                                //
   Serial.print(F(__TIME__));                                                  //                                  //
   Serial.print(F("\n"));                                                      //                                  //
   while (!MCP7940.begin()) {                                                  // Initialize RTC communications    //
-    Serial.println(F("Unable to find MCP7940M. Checking again in 3s."));      // Show error text                  //
+    Serial.println(F("Unable to find MCP7940. Checking again in 3s."));       // Show error text                  //
     delay(3000);                                                              // wait a second                    //
   } // of loop until device is located                                        //                                  //
   Serial.println(F("MCP7940 initialized."));                                  //                                  //
