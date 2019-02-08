f56932a49a56491d5e60781f11c7c35d7899e298
Travis CI <travis@travis-ci.org>
2019-02-08 06:56:30 +0000

    Deploy code docs to GitHub Pages Travis build: 72
    Commit: 7567dfd5e08d9f231f857feb777056d07a1d6567

--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -8,6 +8,8 @@ var searchData=
   ['getpowerdown',['getPowerDown',['../class_m_c_p7940___class.html#a73fa5b2c7c3b1049a6c699b9cf9015a4',1,'MCP7940_Class']]],
   ['getpowerfail',['getPowerFail',['../class_m_c_p7940___class.html#a462ac402901cba5ff2901b37948bb638',1,'MCP7940_Class']]],
   ['getpowerup',['getPowerUp',['../class_m_c_p7940___class.html#a4db17db27e5cceb4b18e8e163900298b',1,'MCP7940_Class']]],
+  ['getppmdeviation',['getPPMDeviation',['../class_m_c_p7940___class.html#a48c0bb3021886d6045c237eb5882fe1f',1,'MCP7940_Class']]],
+  ['getsetunixtime',['getSetUnixTime',['../class_m_c_p7940___class.html#aa2a131b2f8902fd7d93c573fa38b7441',1,'MCP7940_Class']]],
   ['getsqwspeed',['getSQWSpeed',['../class_m_c_p7940___class.html#a904bd8332348bfd5fb4bc6f95c6ca174',1,'MCP7940_Class']]],
   ['getsqwstate',['getSQWState',['../class_m_c_p7940___class.html#ada7534679a00762a5f23eeaa8d6cf6d0',1,'MCP7940_Class']]]
 ];
--- html/search/functions_c.js
+++ html/search/functions_c.js
@@ -8,6 +8,7 @@ var searchData=
   ['setalarmstate',['setAlarmState',['../class_m_c_p7940___class.html#a951782d71c5a81054a2f78999ca6f13c',1,'MCP7940_Class']]],
   ['setbattery',['setBattery',['../class_m_c_p7940___class.html#ac21ac475c546c6cfd2696e83b15a95f9',1,'MCP7940_Class']]],
   ['setmfp',['setMFP',['../class_m_c_p7940___class.html#a4c20fddd9bf29e500c1174e413ab5f0c',1,'MCP7940_Class']]],
+  ['setsetunixtime',['setSetUnixTime',['../class_m_c_p7940___class.html#a021dab49f0ed4b86ba48f1af330cdedb',1,'MCP7940_Class']]],
   ['setsqwspeed',['setSQWSpeed',['../class_m_c_p7940___class.html#aa94b80f7877f2630cb4780389bb8293f',1,'MCP7940_Class']]],
   ['setsqwstate',['setSQWState',['../class_m_c_p7940___class.html#a7bbf50ae5568263d231f80c88b586e9b',1,'MCP7940_Class']]],
   ['setup',['setup',['../_access_memory_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;AccessMemory.ino'],['../_set_alarms_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;SetAlarms.ino']]]
