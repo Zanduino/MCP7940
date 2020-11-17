a6d7b403acb3ee4fbf0697388aa8eeaaf71175b3
Arnd <Arnd_Github@SV-Zanshin.Com>
2020-11-17 10:12:00 +0100

    Issue #50

--- src/MCP7940.h
+++ src/MCP7940.h
@@ -88,7 +88,7 @@ class TimeSpan;
 ***************************************************************************************************/
 #ifndef I2C_MODES  // I2C related constants
 /** @brief Guard code definition */
-#define I2C_MODES  // Guard code to prevent multiple definitions
+#define I2C_MODES
 const uint32_t I2C_STANDARD_MODE = 100000;  ///< Default normal I2C 100KHz speed
 const uint32_t I2C_FAST_MODE     = 400000;  ///< Fast mode
 #endif
